5baf2770dbe47661d7ebd761eddd0f08419793f6
Noah Tonnesen <noah.tonnesen@gmail.com>
2024-03-04 17:06:28 -0500

    menu placeholder

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        do {
+            System.out.println("\nMenu:");
+            System.out.println("1. Task 1");
+            System.out.println("2. Task 2");
+            System.out.println("3. Task 3");
+            System.out.println("4. Quit");
 
-            NOAH
+            System.out.print("Enter your choice: ");
+            option = scanner.nextInt();
 
-            test2
-        }
+            switch (option) {
+                case 1:
+                    System.out.println("You selected option 1");
+                    // Add your code for option 1 here
+                    break;
+                case 2:
+                    System.out.println("You selected option 2");
+                    // Add your code for option 2 here
+                    break;
+                case 3:
+                    System.out.println("You selected option 3");
+                    // Add your code for option 3 here
+                    break;
+                case 4:
+                    System.out.println("Exiting the program...");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while (option != 4);
+
+        scanner.close();
     }
 }
