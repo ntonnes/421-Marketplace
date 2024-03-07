fd92128776ea0373e5c53324d2f9f1ad8f3c225e
Noah Tonnesen <noah.tonnesen@gmail.com>
2024-03-07 18:24:11 -0500

    started Task 2

---
+++ src/Task2.java
@@ -0,0 +1,12 @@
+// Task 2 - Noah
+
+// SEARCH or BROWSE and ADD TO CART
+// When the task is selected, the user is asked to optionally select a category and brand
+// The user is then presented with a list of items that match the category and brand
+// If no category or brand is selected, the user is presented with a list of all items, sorted by model rating
+// The user can then add items to their cart
+
+
+public class Task2 {
+    
+}
